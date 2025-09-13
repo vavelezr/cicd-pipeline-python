5a397055fe694798f18532f77d520da2fa693558
velezvanessa <velezvanessa193@gmail.com>
2025-09-12 22:13:07 -0500

    Implement CI pipeline with Docker build and push

---
+++ app/__init__.py
@@ -0,0 +1,2 @@
+# app/__init__.py
+# Archivo vacío para marcar 'app' como un paquete Python
---
+++ app/calculadora.py
@@ -0,0 +1,17 @@
+# app/calculadora.py
+def sumar(a, b):
+    return a + b
+
+
+def restar(a, b):
+    return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+
+def dividir(a, b):
+    if b == 0:
+        raise ZeroDivisionError("No se puede dividir por cero")
+    return a / b
