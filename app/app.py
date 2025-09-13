0142df43b6f85140c1aa25506630013815f55459
velezvanessa <velezvanessa193@gmail.com>
2025-09-13 09:01:33 -0500

    fix security http hotspot

--- app/app.py
+++ app/app.py
@@ -8,30 +8,34 @@ from .calculadora import sumar, restar, multiplicar, dividir
 app = Flask(__name__)
 
 
-@app.route("/sensitive", methods=["GET", "POST"])
+@app.get("/")
+def index_get():
+    """Funcion principal de la app."""
+    return render_template("index.html")
+
+
+@app.post("/")
 def index():
     """Funcion principal de la app."""
     resultado = None
-    if request.method == "POST":
-        try:
-            num1 = float(request.form["num1"])
-            num2 = float(request.form["num2"])
-            operacion = request.form["operacion"]
-
-            if operacion == "sumar":
-                resultado = sumar(num1, num2)
-            elif operacion == "restar":
-                resultado = restar(num1, num2)
-            elif operacion == "multiplicar":
-                resultado = multiplicar(num1, num2)
-            elif operacion == "dividir":
-                resultado = dividir(num1, num2)
-            else:
-                resultado = "Operación no válida"
-        except ValueError:
-            resultado = "Error: Introduce números válidos"
-        except ZeroDivisionError:
-            resultado = "Error: No se puede dividir por cero"
+    try:
+        num1 = float(request.form["num1"])
+        num2 = float(request.form["num2"])
+        operacion = request.form["operacion"]
+        if operacion == "sumar":
+            resultado = sumar(num1, num2)
+        elif operacion == "restar":
+            resultado = restar(num1, num2)
+        elif operacion == "multiplicar":
+            resultado = multiplicar(num1, num2)
+        elif operacion == "dividir":
+            resultado = dividir(num1, num2)
+        else:
+            resultado = "Operación no válida"
+    except ValueError:
+        resultado = "Error: Introduce números válidos"
+    except ZeroDivisionError:
+        resultado = "Error: No se puede dividir por cero"
 
     return render_template("index.html", resultado=resultado)
 
