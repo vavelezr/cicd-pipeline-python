f7dc6916958e7231ea6a130ce4ff932dc6250519
velezvanessa <velezvanessa193@gmail.com>
2025-09-13 08:19:09 -0500

    run black app

--- app/app.py
+++ app/app.py
@@ -37,6 +37,4 @@ def index():
 
 
 if __name__ == "__main__":  # pragma: no cover
-    app.run(debug=True,
-            port=5000,
-            host="0.0.0.0")  # Quita debug=True para producción
+    app.run(debug=True, port=5000, host="0.0.0.0")  # Quita debug=True para producción
